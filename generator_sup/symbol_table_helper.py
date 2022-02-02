f6400e14e1d2c130d88a5d5aacf7e58b9f5574e2
mtndaghyani <matin.daghyani@gmail.com>
2022-02-02 09:51:19 +0330

    Change add_to_table parameter name because it shadowed the keyword 'input'

--- generator_sup/symbol_table_helper.py
+++ generator_sup/symbol_table_helper.py
@@ -2,5 +2,5 @@ class SymbolTable:
     def __init__(self) -> None:
         self.table = []
         
-    def add_to_table(self, input):
-        self.table.append(input)
+    def add_to_table(self, row):
+        self.table.append(row)
