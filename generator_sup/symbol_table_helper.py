7905808fcda933572ad4950985c39b15e909acee
mtndaghyani <matin.daghyani@gmail.com>
2022-02-02 10:12:42 +0330

    Add some helpful methods to symbol_table

--- generator_sup/symbol_table_helper.py
+++ generator_sup/symbol_table_helper.py
@@ -4,3 +4,17 @@ class SymbolTable:
         
     def add_to_table(self, row):
         self.table.append(row)
+
+    def get_address(self, identifier):
+        for row in self.table[::-1]:
+            if row[0] == identifier:
+                return row[2]
+        raise Exception('ID not found in symbol table.')
+
+    @property
+    def size(self):
+        return len(self.table)
+
+    def collapse_symbol_table(self, to_keep):
+        # Pop until the length of table becomes equal to 'remained'
+        self.table = self.table[:to_keep]
